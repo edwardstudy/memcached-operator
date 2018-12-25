aabb61df2eb2f9628beed70051ab230a2dffa9b0
Zida <shawzida@gmail.com>
2018-12-25 23:23:59 +0800

    Add informer

--- pkg/controller/informer.go
+++ pkg/controller/informer.go
@@ -1,14 +1,4 @@
 package controller
 
-import (
-	"k8s.io/apimachinery/pkg/fields"
-	"k8s.io/client-go/tools/cache"
-)
-
 func Start(){
-	source := cache.NewListWatchFromClient(
-		c.Config.EtcdCRCli.EtcdV1beta2().RESTClient(),
-		api.EtcdClusterResourcePlural,
-		ns,
-		fields.Everything())
 }
---
+++ pkg/controller/memcached/imformer.go
@@ -0,0 +1,13 @@
+package memcached
+
+
+func Start(){
+	// TODO Init CRD
+
+	// TODO Run controller
+}
+
+
+func run(){
+	// TODO Use workqueue
+}
